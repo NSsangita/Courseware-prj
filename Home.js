e3212a7f370a27aa0cfe1fe472f65b5b7f4e4d92
NSsangita <131617462+NSsangita@users.noreply.github.com>
2023-12-03 14:33:17 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,21 +1,49 @@
-import React from 'react';
-import { Container,Button } from 'reactstrap';
+import React, { useEffect } from 'react';
+import { Container,Button} from 'reactstrap';
+import bg from "../images/bg.webp";
+import Heading from './Heading';
+import { Link} from 'react-router-dom';
+import "./Home.css";
+import Head from './Head';
+
+
+
 const Home = () => {
+
+  useEffect(()=>{
+    document.title = "Home || Learncode With SSS-M";
+  },[]);
+
   return (
-    <div className="text-center bg-info" font-weight-bold>
+    <>
+    <Head/>
+    <section className='hero'>
+     <div className='container'>
+        <div className='row'>
+       <b><Heading subtitle='WELCOME TO ACADEMIC' title='Best Online Education Expertise' /></b> 
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className='button'>
+          <Link to="/">
+            <button className='primary-btn'>
+              GET STARTED NOW <i class="fa-solid fa-arrow-right"></i>
+            </button>
+            </Link>
+          <Link to="/loginform">
+            <button>
+              VIEW COURSE <i class="fa-solid fa-arrow-right"></i>
+            </button>
+            </Link>
+          </div>
+        </div>
+       <b> <p className='font-bold text-sm tracking-wide text-gray-800 mt-2'>@centurionuniversity.com.nic bbsr campus</p></b>
+
+      </div>
+    </section>
     
-      <h2>Learn code with Chunnu</h2>
-
-    <p>This is developed by Chunnu for learning purpose.It is backend is on spring boot and frontened on reactjs.</p>
-
-    <Container className="text-center" style={{height:120}}>
-
-    <Button color="primary" outline>Start using my application</Button>
-
-    </Container>
-   
-    </div>
-  )
+    <div className='margin'></div>
+  </>
+)
 }
+
 
 export default Home;
